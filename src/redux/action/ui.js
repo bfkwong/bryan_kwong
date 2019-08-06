25b710101360b13aac3f3ebde614b2255a342380
Bryan Kwong <bfkwong@calpoly.edu>
2019-08-05 23:05:39 -0700

    Updates

--- src/redux/action/ui.js
+++
@@ -1,9 +0,0 @@
-export const TOGGLE_PROFESSION_MODE = "TOGGLE_PROFESSION_MODE";
-
-export const toggleProfessionaMode = professionalModeId => {
-    return {
-        type: TOGGLE_PROFESSION_MODE,
-        meta: null,
-        payload: professionalModeId
-    };
-};
--- src/redux/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import uiReducer from "./uiReducer";
-import enumReducer from "./enumReducer";
-
-export default combineReducers({
-    Ui: uiReducer,
-    Enum: enumReducer
-});
